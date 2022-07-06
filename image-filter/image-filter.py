bcb7ddc30560969aeb4d0424c397ac9bfce8a925
asad <asadnabi7@gmail.com>
2022-07-06 16:55:18 -0400

    added comments

--- image-filter/image-filter.py
+++ image-filter/image-filter.py
@@ -1,32 +1,45 @@
 import sys
 from PIL import Image, ImageFilter, ImageOps
 
+'''
+
+input:(format in command line argument) image-filter.py imgfile-name filter-name
+
+output: transformed images based on selected filter
+
+'''
+
 
 # Options for filter: greyscale, flip, invert, mirror, blur, smooth
 opt_list = ["grayscale", "flip", "invert", "mirror", "blur", "smooth"]
 
-
+# Transforms image to grayscale rgb values (50,50,50)
 def grayscale(img):
     new_img = ImageOps.grayscale(img)
     return new_img
 
+# Flips pixel orientation in image
 def flip(img):
     new_img = ImageOps.flip(img)
     return new_img
 
+# Inverts pixel orientation in image
 def invert(img):
     new_img = ImageOps.invert(img)
     return new_img
 
+# Mirrors pixel orientation in image
 def mirror(img):
     new_img = ImageOps.mirror(img)
     return new_img
 
+# Reduces pixels via BoxBlur -  works by taking each pixel and, giving it a color value that is the average of it's neighbors
 def blur(img):
     blurlevel = int(input("Blur level: "))
     new_img = img.filter(ImageFilter.BoxBlur(blurlevel))
     return new_img
 
+# smoothens image thereby reducing sharpness
 def smooth(img):
     choice = 0
     while True:
@@ -39,7 +52,7 @@ def smooth(img):
         new_img = img.filter(ImageFilter.SMOOTH_MORE)
     return new_img
 
-
+# ensures that user is adhering to arguments
 def argv_check(argv):
     if len(argv) != 3:
         print("\nerror - input command-line argument must be three \n\nformat: \n\"image-filter.py imgfile-name filter-name\"\n")
@@ -51,6 +64,9 @@ def argv_check(argv):
         sys.exit(1)
 
 
+# main function below
+
+# checks if command argument is valid
 argv_check(sys.argv)
 try:
     img = Image.open(sys.argv[1])
@@ -58,6 +74,7 @@ except FileNotFoundError:
     print("\nError - File not found \n\nMake sure file name is correct and in the script's directory\n")
     sys.exit(2)
 
+# selects the filter chosen by the user
 if sys.argv[2] == 'grayscale':
     new_img = grayscale(img)
 if sys.argv[2] == 'flip':
@@ -71,6 +88,7 @@ if sys.argv[2] == 'blur':
 if sys.argv[2] == 'smooth':
     new_img = smooth(img) 
 
+# saves image and gives image name as user input
 print("Saving Image ...")
 imagename = input("Image Name: ")
 new_img.save(imagename)
